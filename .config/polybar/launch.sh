e487cff1b4ecec57e350b4cafeb757068ec99593
Italo Borrelli <ignborrelli00@proton.me>
2023-10-19 11:12:09 -0700

    Add updates to configs

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,4 +7,13 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar 
-polybar -c ~/.config/polybar/config.ini bottom &
+#polybar -c ~/.config/polybar/config.ini bottom &
+
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m /home/iborrelli/polybar/build/bin/polybar -c ~/.config/polybar/config.ini bottom &
+		MONITOR=$m /home/iborrelli/polybar/build/bin/polybar -c ~/.config/polybar/config.ini top &
+	done
+else
+	/home/iborrelli/polybar/build/bin/polybar -c ~/.config/polybar/config.ini bottom &
+fi
--- .config/polybar/scripts/window-title.sh
+++ .config/polybar/scripts/window-title.sh
@@ -9,6 +9,34 @@ elif [ -z "$windowid" ]
 then
     echo 'Arch'
 else
-    preprint="$(xprop -id $windowid WM_CLASS | awk '{print $4}' | tr -d '"')"
-    echo ${preprint}
+    class=$(xprop -id $windowid WM_CLASS | awk -F '"' '{print $2}')
+    title="$(xprop -id $windowid WM_NAME | awk -F '"' '{print $2}')"
+    short_title=$(basename "$title")
+
+    if [[ "$class" == *"google-chrome"* ]] && [[ "$class" == *"Cypress"* ]]; then
+	    echo "Cypress"
+    elif [[ "$title" == *"VIM"* ]]; then
+	    vim_string=$(echo "$title" | sed 's/ - VIM//')
+	    if [ ${#vim_string} -le 50 ]; then
+		    name=$vim_string
+	    else
+		    name="$(basename $vim_string)"
+	    fi
+    elif [ "$class" == "kitty" ]; then
+	    name="$title"
+    elif [ "$class" == "code" ]; then
+	    name="VS Code"
+    else
+    	name="$class"
+    fi
+    #add spaces and trim to exactly 50 characters
+    #with_spaces="$name                                                  "
+    #echo "${with_spaces:0:50}"
+    padding_length=$(echo "scale=0; (50 - ${#name}) / 2" | bc)
+    padding_spaces=$(printf '%*s' "$padding_length" '')
+    centered_string="$padding_spaces$name$padding_spaces"
+    if [ $(( ${#centered_string} % 2 )) -eq 1 ]; then
+	    centered_string="${centered_string} "
+    fi
+    echo "$centered_string"
 fi
